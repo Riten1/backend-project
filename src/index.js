e3481762484473d72b3f6a7d60dee6854ba0cc30
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-10-20 10:07:03 +0545

    feat: Adding custom api error and response code.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { connectDB } from "./db/index.js";
 import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() =>
+    app.listen(process.env.PORT || 8000, () => console.log("MongoDB connected"))
+  )
+  .catch((err) => console.log("MongoDB connection error: ", err));
