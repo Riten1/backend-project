a0c2e09d7f09bd5b83d4e8585d70027a0baff65b
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-10-27 20:11:49 +0545

    feat: add update account detail controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -226,4 +226,44 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse({}, 200, "Password changed successfully"));
 });
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  return res.status(200).json(new ApiResponse({ user }, 200, "User found"));
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse({ user }, 200, "Account details updated successfully")
+    );
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changeCurrentPassword,
+};
